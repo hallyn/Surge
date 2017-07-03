3185548a264cf54cd9566c4bbb12d362564ee802
Serge Hallyn <serge@hallyn.com>
2017-07-03 00:21:46 -0500

    Add license
    I probably want a more free license, but let's start with this.
    
    Signed-off-by: Serge Hallyn <serge@hallyn.com>

--- surge.c
+++ surge.c
@@ -1,3 +1,14 @@
+/*
+ * Surge init
+ *
+ * Copyright (C) Serge Hallyn <serge@hallyn.com>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ */
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
